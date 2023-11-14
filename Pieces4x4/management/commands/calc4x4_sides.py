ec6119f9ac107c68b93b3ce44a3c416a51d8acca
RamonvdW <nhb.ramonvdw@gmail.com>
2023-11-14 07:28:21 +0100

    Speed up

--- Pieces4x4/management/commands/calc4x4_sides.py
+++ Pieces4x4/management/commands/calc4x4_sides.py
@@ -6,7 +6,6 @@
 
 from django.core.management.base import BaseCommand
 from BasePieces.models import BasePiece
-from Pieces2x2.models import TwoSides, Piece2x2
 from Pieces4x4.models import Piece4x4
 
 USE_CACHE = True
@@ -30,8 +29,12 @@ class Command(BaseCommand):
 
         done = 0
         print_done = 10000
-        for p in Piece4x4.objects.all().iterator(chunk_size=10000):
 
+        # find a piece without a side
+        p = Piece4x4.objects.filter(side2='', side3='').first()
+        while p:
+
+            # calculate the sides
             side2 = self.nr2base[p.nr4].get_side(2, p.rot4)
             side2 += self.nr2base[p.nr8].get_side(2, p.rot8)
             side2 += self.nr2base[p.nr12].get_side(2, p.rot12)
@@ -42,14 +45,20 @@ class Command(BaseCommand):
             side3 += self.nr2base[p.nr14].get_side(3, p.rot14)
             side3 += self.nr2base[p.nr13].get_side(3, p.rot13)
 
-            p.side2 = side2
-            p.side3 = side3
-
-            p.save(update_fields=['side2', 'side3'])
             done += 1
-            if done >= print_done:
-                self.stdout.write('%s done' % done)
-                print_done += 10000
-        # for
+            self.stdout.write('[INFO] Update %s: side2=%s, side3=%s' % (done, side2, side3))
+
+            # update all the Piece4x4 with these borders in one go
+            (Piece4x4
+             .objects
+             .filter(nr4=p.nr4, nr8=p.nr8, nr12=p.nr12, nr13=p.nr13, nr14=p.nr14, nr15=p.nr15, nr16=p.nr16,
+                     rot4=p.rot4, rot8=p.rot8, rot12=p.rot12, rot13=p.rot13, rot14=p.rot14, rot15=p.rot15,rot16=p.rot16)
+             .update(side2=side2, side3=side3))
+             
+            # find the next piece without a side
+            p = Piece4x4.objects.filter(side2='', side3='').first()
+        # while
+
+        self.stdout.write('[INFO] Done!')
 
 # end of file
