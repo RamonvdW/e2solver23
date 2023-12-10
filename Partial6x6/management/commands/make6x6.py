d7990fba8848b2736e42397dbd990cbcfff43351
RamonvdW <nhb.ramonvdw@gmail.com>
2023-12-10 22:02:43 +0100

    Corrections

--- Partial6x6/management/commands/make6x6.py
+++ Partial6x6/management/commands/make6x6.py
@@ -17,15 +17,6 @@ class Command(BaseCommand):
 
     help = "Partial6x6 generator using Quart6"
 
-    NR_IGNORE = (1, 2, 3, 4, 5, 6, 7, 8,
-                 9, 16, 17, 24, 25, 32, 33, 40, 41, 48, 49, 56,
-                 57, 58, 59, 60, 61, 62, 63, 64)
-
-    NRS_4x4_C1 = (27, 19, 20)
-    NRS_4x4_C2 = (21, 22, 30)
-    NRS_4x4_C3 = (38, 46, 45)
-    NRS_4x4_C4 = (44, 43, 35)
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.verbose = 0
@@ -38,7 +29,7 @@ class Command(BaseCommand):
         self.interval_mins = 15
         self.my_processor = int(time.time() - 946684800.0)     # seconds since Jan 1, 2000
 
-        self.rev_border = TwoSides.objects.get(two_sides='XX').nr
+        self.two_border = TwoSides.objects.get(two_sides='XX').nr
         self.side_nr2reverse = dict()
         two2nr = dict()
         for two in TwoSides.objects.all():
@@ -62,37 +53,37 @@ class Command(BaseCommand):
 
         if nr in NRS_CORNER:
             if nr == 1:
-                s1 = s4 = self.rev_border
+                s1 = s4 = self.two_border
             elif nr == 8:
-                s1 = s2 = self.rev_border
+                s1 = s2 = self.two_border
             elif nr == 57:
-                s3 = s4 = self.rev_border
+                s3 = s4 = self.two_border
             else:
-                s2 = s3 = self.rev_border
+                s2 = s3 = self.two_border
 
         elif nr in NRS_BORDER:
             if nr < 9:
-                s1 = self.rev_border
-                x2 = self.rev_border
-                x4 = self.rev_border
+                s1 = self.two_border
+                x2 = self.two_border
+                x4 = self.two_border
 
             elif nr > 57:
-                x2 = self.rev_border
-                s3 = self.rev_border
-                x4 = self.rev_border
+                x2 = self.two_border
+                s3 = self.two_border
+                x4 = self.two_border
 
             elif nr & 1 == 1:
-                x1 = self.rev_border
-                x3 = self.rev_border
-                s4 = self.rev_border
+                x1 = self.two_border
+                x3 = self.two_border
+                s4 = self.two_border
 
             else:
-                x1 = self.rev_border
-                s2 = self.rev_border
-                x3 = self.rev_border
+                x1 = self.two_border
+                s2 = self.two_border
+                x3 = self.two_border
 
         else:
-            x1 = x2 = x3 = x4 = self.rev_border
+            x1 = x2 = x3 = x4 = self.two_border
 
             if nr in NRS_HINTS:
                 # has_hint = True
@@ -292,7 +283,7 @@ class Command(BaseCommand):
 
         # get the sides
         s1, s2, s3, s4 = self._get_sides(nr)
-        x1 = x2 = x3 = x4 = self.rev_border
+        x1 = x2 = x3 = x4 = self.two_border
         p1 = p2 = p3 = p4 = None
 
         if nr in NRS_HINTS:
@@ -543,7 +534,7 @@ class Command(BaseCommand):
 
         unused_nrs = self.board_unused_nrs
 
-        qset = Piece2x2.objects.filter(side1=self.rev_border)
+        qset = Piece2x2.objects.filter(side1=self.two_border)
         qset = qset.filter(nr1__in=unused_nrs, nr2__in=unused_nrs, nr3__in=unused_nrs, nr4__in=unused_nrs)
 
         """
@@ -604,8 +595,8 @@ class Command(BaseCommand):
         p19 = self.board[19]
         for p11 in self._iter_no_border(11, side4=p10.exp_s4, side3=p19.exp_s3):
             done = False
-            for p2 in self._iter_border(2, side1=self.rev_border, side3=p10.exp_s3):
-                for _ in self._iter_border(3, side1=self.rev_border, side4=p2.exp_s4, side3=p11.exp_s3):
+            for p2 in self._iter_border(2, side1=self.two_border, side3=p10.exp_s3):
+                for _ in self._iter_border(3, side1=self.two_border, side4=p2.exp_s4, side3=p11.exp_s3):
                     nrs.append(p11.nr)
                     done = True
                     break
@@ -614,7 +605,7 @@ class Command(BaseCommand):
                     break
             # for
         # for
-        return set(nrs)
+        return nrs
 
     def _get_candidates_p14(self):
         nrs = list()
@@ -622,8 +613,8 @@ class Command(BaseCommand):
         p22 = self.board[22]
         for p14 in self._iter_no_border(14, side2=p15.exp_s2, side3=p22.exp_s3):
             done = False
-            for p7 in self._iter_border(7, side1=self.rev_border, side3=p15.exp_s3):
-                for _ in self._iter_border(6, side1=self.rev_border, side2=p7.exp_s2, side3=p14.exp_s3):
+            for p7 in self._iter_border(7, side1=self.two_border, side3=p15.exp_s3):
+                for _ in self._iter_border(6, side1=self.two_border, side2=p7.exp_s2, side3=p14.exp_s3):
                     nrs.append(p14.nr)
                     done = True
                     break
@@ -632,7 +623,7 @@ class Command(BaseCommand):
                     break
             # for
         # for
-        return set(nrs)
+        return nrs
 
     def _get_candidates_p18(self):
         nrs = list()
@@ -640,8 +631,8 @@ class Command(BaseCommand):
         p19 = self.board[19]
         for p18 in self._iter_no_border(18, side1=p10.exp_s1, side2=p19.exp_s2):
             done = False
-            for p9 in self._iter_border(9, side4=self.rev_border, side2=p10.exp_s2):
-                for _ in self._iter_border(17, side4=self.rev_border, side1=p9.exp_s1, side2=p18.exp_s2):
+            for p9 in self._iter_border(9, side4=self.two_border, side2=p10.exp_s2):
+                for _ in self._iter_border(17, side4=self.two_border, side1=p9.exp_s1, side2=p18.exp_s2):
                     nrs.append(p18.nr)
                     done = True
                     break
@@ -650,7 +641,7 @@ class Command(BaseCommand):
                     break
             # for
         # for
-        return set(nrs)
+        return nrs
 
     def _get_candidates_p23(self):
         nrs = list()
@@ -658,8 +649,8 @@ class Command(BaseCommand):
         p22 = self.board[22]
         for p23 in self._iter_no_border(23, side1=p15.exp_s1, side4=p22.exp_s4):
             done = False
-            for p16 in self._iter_border(16, side2=self.rev_border, side4=p15.exp_s4):
-                for _ in self._iter_border(24, side2=self.rev_border, side1=p16.exp_s1, side4=p23.exp_s4):
+            for p16 in self._iter_border(16, side2=self.two_border, side4=p15.exp_s4):
+                for _ in self._iter_border(24, side2=self.two_border, side1=p16.exp_s1, side4=p23.exp_s4):
                     nrs.append(p23.nr)
                     done = True
                     break
@@ -668,7 +659,7 @@ class Command(BaseCommand):
                     break
             # for
         # for
-        return set(nrs)
+        return nrs
 
     def _get_candidates_p42(self):
         nrs = list()
@@ -676,8 +667,8 @@ class Command(BaseCommand):
         p43 = self.board[43]
         for p42 in self._iter_no_border(42, side3=p50.exp_s3, side2=p43.exp_s2):
             done = False
-            for p49 in self._iter_border(49, side4=self.rev_border, side2=p50.exp_s2):
-                for _ in self._iter_border(41, side4=self.rev_border, side3=p49.exp_s3, side2=p42.exp_s2):
+            for p49 in self._iter_border(49, side4=self.two_border, side2=p50.exp_s2):
+                for _ in self._iter_border(41, side4=self.two_border, side3=p49.exp_s3, side2=p42.exp_s2):
                     nrs.append(p42.nr)
                     done = True
                     break
@@ -686,7 +677,7 @@ class Command(BaseCommand):
                     break
             # for
         # for
-        return set(nrs)
+        return nrs
 
     def _get_candidates_p47(self):
         nrs = list()
@@ -694,8 +685,8 @@ class Command(BaseCommand):
         p46 = self.board[46]
         for p47 in self._iter_no_border(47, side3=p55.exp_s3, side4=p46.exp_s4):
             done = False
-            for p56 in self._iter_border(56, side2=self.rev_border, side4=p55.exp_s4):
-                for _ in self._iter_border(48, side2=self.rev_border, side3=p56.exp_s3, side4=p47.exp_s4):
+            for p56 in self._iter_border(56, side2=self.two_border, side4=p55.exp_s4):
+                for _ in self._iter_border(48, side2=self.two_border, side3=p56.exp_s3, side4=p47.exp_s4):
                     nrs.append(p47.nr)
                     done = True
                     break
@@ -704,7 +695,7 @@ class Command(BaseCommand):
                     break
             # for
         # for
-        return set(nrs)
+        return nrs
 
     def _get_candidates_p51(self):
         nrs = list()
@@ -712,8 +703,8 @@ class Command(BaseCommand):
         p43 = self.board[43]
         for p51 in self._iter_no_border(51, side4=p50.exp_s4, side1=p43.exp_s1):
             done = False
-            for p58 in self._iter_border(58, side3=self.rev_border, side1=p50.exp_s1):
-                for _ in self._iter_border(59, side3=self.rev_border, side4=p58.exp_s4, side1=p51.exp_s3):
+            for p58 in self._iter_border(58, side3=self.two_border, side1=p50.exp_s1):
+                for _ in self._iter_border(59, side3=self.two_border, side4=p58.exp_s4, side1=p51.exp_s3):
                     nrs.append(p51.nr)
                     done = True
                     break
@@ -722,7 +713,7 @@ class Command(BaseCommand):
                     break
             # for
         # for
-        return set(nrs)
+        return nrs
 
     def _get_candidates_p54(self):
         nrs = list()
@@ -730,8 +721,8 @@ class Command(BaseCommand):
         p46 = self.board[46]
         for p54 in self._iter_no_border(54, side2=p55.exp_s2, side1=p46.exp_s1):
             done = False
-            for p63 in self._iter_border(63, side3=self.rev_border, side1=p55.exp_s1):
-                for _ in self._iter_border(62, side3=self.rev_border, side2=p63.exp_s2, side1=p54.exp_s1):
+            for p63 in self._iter_border(63, side3=self.two_border, side1=p55.exp_s1):
+                for _ in self._iter_border(62, side3=self.two_border, side2=p63.exp_s2, side1=p54.exp_s1):
                     nrs.append(p54.nr)
                     done = True
                     break
@@ -740,7 +731,7 @@ class Command(BaseCommand):
                     break
             # for
         # for
-        return set(nrs)
+        return nrs
 
     def _get_candidates_p1(self, nr_c):
         if nr_c == 10:
@@ -775,80 +766,68 @@ class Command(BaseCommand):
             self._board_fill_nr(nr_c, c1)
 
             nrs_p1 = self._get_candidates_p1(nr_c)
-            if len(nrs_p1) == 0:
-                continue
+            if len(nrs_p1) > 0:
 
-            nrs_p2 = self._get_candidates_p2(nr_c)
-            if len(nrs_p2) == 0:
-                continue
+                nrs_p2 = self._get_candidates_p2(nr_c)
+                if len(nrs_p2) > 0:
 
-            todo = len(nrs_p1) * len(nrs_p2)
-            done1 = 0
-            print_perc = 1.0
+                    for p1 in self._iter_nrs_no_borders(nrs_p1):
+                        self._board_fill_nr(nr_p1, p1)
 
-            for p1 in self._iter_nrs_no_borders(nrs_p1):
-                self._board_fill_nr(nr_p1, p1)
+                        for p2 in self._iter_nrs_no_borders(nrs_p2):
+                            self._board_fill_nr(nr_p2, p2)
 
-                done2 = done1
-                for p2 in self._iter_nrs_no_borders(nrs_p2):
-                    self._board_fill_nr(nr_p2, p2)
+                            if self._can_fill_nrs_no_borders(nrs_chk):
 
-                    if self._can_fill_nrs_no_borders(nrs_chk):
+                                if self._can_fill_nrs_border(nr_c):
 
-                        if self._can_fill_nrs_border(nr_c):
+                                    quart = Quart6(
+                                                processor=self.my_processor,
+                                                based_on_4x4=self.based_on,
+                                                type=nr_c,
+                                                p1=p1.pk,
+                                                c1=c1.pk,
+                                                p2=p2.pk,
+                                                nr1=p1.nr1,
+                                                nr2=p1.nr2,
+                                                nr3=p1.nr3,
+                                                nr4=p1.nr4,
+                                                nr5=c1.nr1,
+                                                nr6=c1.nr2,
+                                                nr7=c1.nr3,
+                                                nr8=c1.nr4,
+                                                nr9=p2.nr1,
+                                                nr10=p2.nr2,
+                                                nr11=p2.nr3,
+                                                nr12=p2.nr4)
 
-                            quart = Quart6(
-                                        processor=self.my_processor,
-                                        based_on_4x4=self.based_on,
-                                        type=nr_c,
-                                        p1=p1.pk,
-                                        c1=c1.pk,
-                                        p2=p2.pk,
-                                        nr1=p1.nr1,
-                                        nr2=p1.nr2,
-                                        nr3=p1.nr3,
-                                        nr4=p1.nr4,
-                                        nr5=c1.nr1,
-                                        nr6=c1.nr2,
-                                        nr7=c1.nr3,
-                                        nr8=c1.nr4,
-                                        nr9=p2.nr1,
-                                        nr10=p2.nr2,
-                                        nr11=p2.nr3,
-                                        nr12=p2.nr4)
+                                    bulk.append(quart)
+                                    if len(bulk) >= 25:
+                                        count += len(bulk)
+                                        print('Quart6 type %s: %s (reject1 %s, reject2 %s)' % (
+                                                nr_c, count, reject1_count, reject2_count))
+                                        Quart6.objects.bulk_create(bulk)
+                                        bulk = list()
+                                else:
+                                    reject1_count += 1
 
-                            bulk.append(quart)
-                            if len(bulk) >= 25:
-                                count += len(bulk)
-                                print('Quart6 type %s: %s (reject1 %s, reject2 %s) done1=%s, done2=%s, todo=%s' % (nr_c, count, reject1_count, reject2_count, done1, done2, todo))
-                                Quart6.objects.bulk_create(bulk)
-                                bulk = list()
-                        else:
-                            reject1_count += 1
+                            else:
+                                reject2_count += 1
 
-                    else:
-                        reject2_count += 1
+                            self._board_free_nr(nr_p2)
+                        # for
 
-                    done2 += 1
-                    perc = (done2 * 100) / todo
-                    if perc >= print_perc:
-                        print("%.0f%% done" % perc)
-                        print_perc += 1.0
-
-                    self._board_free_nr(nr_p2)
-                # for
-
-                done1 += len(nrs_p2)
-                self._board_free_nr(nr_p1)
-            # for
+                        self._board_free_nr(nr_p1)
+                    # for
 
             self._board_free_nr(nr_c)
         # for
 
         if len(bulk) > 0:
             count += len(bulk)
-            print('Quart6 type %s: %s (reject1 %s, reject2 %s)' % (nr_c, count, reject1_count, reject2_count))
             Quart6.objects.bulk_create(bulk)
+
+        print('Quart6 type %s: %s (reject1 %s, reject2 %s)' % (nr_c, count, reject1_count, reject2_count))
 
     def _generate_all_quart6(self):
         self._generate_quart6(18, 10, 11, (26, 12))
