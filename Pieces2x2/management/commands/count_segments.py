adf20f533650b8c36553fc4c0969a6b105e21235
RamonvdW <nhb.ramonvdw@gmail.com>
2023-12-28 15:57:17 +0100

    Automation

---
+++ Pieces2x2/management/commands/count_segments.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2023 Ramon van der Winkel.
+#  All rights reserved.
+#  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
+
+from django.core.management.base import BaseCommand
+from Pieces2x2.models import TwoSideOptions
+
+
+class Command(BaseCommand):
+
+    help = "Count the TwoSideOptions for a specific processor"
+
+    def add_arguments(self, parser):
+        parser.add_argument('processor', nargs=1, type=int, help='Processor number')
+
+    def handle(self, *args, **options):
+
+        processor = options['processor'][0]
+
+        count = TwoSideOptions.objects.filter(processor=processor).count()
+        self.stdout.write("%s" % count)
+
+# end of file
--- ring1_reduce.sh
+++ ring1_reduce.sh
@@ -1,60 +1,148 @@
 #!/bin/bash
 
 processor=$1
-
 [ -z "$processor" ] && exit 1
 
-./manage.* eval_loc_1 $processor 10
-./manage.* eval_loc_1 $processor 15
-./manage.* eval_loc_1 $processor 50
-./manage.* eval_loc_1 $processor 55
 
-./manage.* eval_loc_1 $processor 11
-./manage.* eval_loc_1 $processor 12
-./manage.* eval_loc_1 $processor 13
-./manage.* eval_loc_1 $processor 14
+scan1()
+{
+    echo "[SCAN PROGRESS] Ring1 scan1 $processor"
+    
+    ./manage.* eval_loc_1 $processor 10
+    ./manage.* eval_loc_1 $processor 11
+    ./manage.* eval_loc_1 $processor 12
+    ./manage.* eval_loc_1 $processor 13
+    ./manage.* eval_loc_1 $processor 14
+    ./manage.* eval_loc_1 $processor 15
+    ./manage.* eval_loc_1 $processor 23
+    ./manage.* eval_loc_1 $processor 31
+    ./manage.* eval_loc_1 $processor 39
+    ./manage.* eval_loc_1 $processor 47
+    ./manage.* eval_loc_1 $processor 55
+    ./manage.* eval_loc_1 $processor 54
+    ./manage.* eval_loc_1 $processor 53
+    ./manage.* eval_loc_1 $processor 52
+    ./manage.* eval_loc_1 $processor 51
+    ./manage.* eval_loc_1 $processor 50
+    ./manage.* eval_loc_1 $processor 42
+    ./manage.* eval_loc_1 $processor 34
+    ./manage.* eval_loc_1 $processor 26
+    ./manage.* eval_loc_1 $processor 18
+}
 
-./manage.* eval_loc_1 $processor 23
-./manage.* eval_loc_1 $processor 31
-./manage.* eval_loc_1 $processor 39
-./manage.* eval_loc_1 $processor 47
+scan2()
+{    
+    echo "[SCAN PROGRESS] Ring1 scan2 $processor"
 
-./manage.* eval_loc_1 $processor 51
-./manage.* eval_loc_1 $processor 52
-./manage.* eval_loc_1 $processor 53
-./manage.* eval_loc_1 $processor 54
+    ./manage.* eval_loc_4 $processor 1
+    ./manage.* eval_loc_4 $processor 2
+    ./manage.* eval_loc_4 $processor 3
+    ./manage.* eval_loc_4 $processor 4
+    ./manage.* eval_loc_4 $processor 5
+    ./manage.* eval_loc_4 $processor 6
+    ./manage.* eval_loc_4 $processor 7
+    ./manage.* eval_loc_4 $processor 15
+    ./manage.* eval_loc_4 $processor 23
+    ./manage.* eval_loc_4 $processor 31
+    ./manage.* eval_loc_4 $processor 39
+    ./manage.* eval_loc_4 $processor 47
+    ./manage.* eval_loc_4 $processor 55
+    ./manage.* eval_loc_4 $processor 54
+    ./manage.* eval_loc_4 $processor 53
+    ./manage.* eval_loc_4 $processor 52
+    ./manage.* eval_loc_4 $processor 51
+    ./manage.* eval_loc_4 $processor 50
+    ./manage.* eval_loc_4 $processor 49
+    ./manage.* eval_loc_4 $processor 41
+    ./manage.* eval_loc_4 $processor 33
+    ./manage.* eval_loc_4 $processor 25
+    ./manage.* eval_loc_4 $processor 17
+    ./manage.* eval_loc_4 $processor 9
+}
 
-./manage.* eval_loc_1 $processor 18
-./manage.* eval_loc_1 $processor 26
-./manage.* eval_loc_1 $processor 34
-./manage.* eval_loc_1 $processor 42
+scan3()
+{
+    echo "[SCAN PROGRESS] Ring1 scan3 $processor"
 
+    ./manage.* eval_loc_1 $processor 19
+    ./manage.* eval_loc_1 $processor 20
+    ./manage.* eval_loc_1 $processor 21
+    ./manage.* eval_loc_1 $processor 22
+    ./manage.* eval_loc_1 $processor 30
+    ./manage.* eval_loc_1 $processor 38
+    ./manage.* eval_loc_1 $processor 46
+    ./manage.* eval_loc_1 $processor 45
+    ./manage.* eval_loc_1 $processor 44
+    ./manage.* eval_loc_1 $processor 43
+    ./manage.* eval_loc_1 $processor 35
+    ./manage.* eval_loc_1 $processor 27
+    ./manage.* eval_loc_1 $processor 19
+    ./manage.* eval_loc_1 $processor 36
+    ./manage.* eval_loc_1 $processor 28
+    ./manage.* eval_loc_1 $processor 37
+}
 
-./manage.* eval_loc_4 $processor 10
-./manage.* eval_loc_1 $processor 11
-./manage.* eval_loc_1 $processor 18
-./manage.* eval_loc_1 $processor 19
+scan4()
+{
+    echo "[SCAN PROGRESS] Ring1 scan4 $processor"
 
-./manage.* eval_loc_4 $processor 14
-./manage.* eval_loc_1 $processor 14
-./manage.* eval_loc_1 $processor 23
-./manage.* eval_loc_1 $processor 22
+    ./manage.* eval_loc_4 $processor 10
+    ./manage.* eval_loc_4 $processor 11
+    ./manage.* eval_loc_4 $processor 12
+    ./manage.* eval_loc_4 $processor 13
 
-./manage.* eval_loc_4 $processor 42
-./manage.* eval_loc_1 $processor 42
-./manage.* eval_loc_1 $processor 51
-./manage.* eval_loc_1 $processor 43
+    ./manage.* eval_loc_4 $processor 14   
+    ./manage.* eval_loc_4 $processor 22
+    ./manage.* eval_loc_4 $processor 30
+    ./manage.* eval_loc_4 $processor 38
+    
+    ./manage.* eval_loc_4 $processor 46
+    ./manage.* eval_loc_4 $processor 45
+    ./manage.* eval_loc_4 $processor 44
+    ./manage.* eval_loc_4 $processor 43
+    
+    ./manage.* eval_loc_4 $processor 42
+    ./manage.* eval_loc_4 $processor 34
+    ./manage.* eval_loc_4 $processor 26
+    ./manage.* eval_loc_4 $processor 18
+}
 
-./manage.* eval_loc_4 $processor 46
-./manage.* eval_loc_1 $processor 47
-./manage.* eval_loc_1 $processor 54
-./manage.* eval_loc_1 $processor 46
+scan5()
+{
+    echo "[SCAN PROGRESS] Ring1 scan5 $processor"
 
-exit
+    ./manage.* eval_loc_4 $processor 19
+    ./manage.* eval_loc_4 $processor 20
+    ./manage.* eval_loc_4 $processor 21
+    ./manage.* eval_loc_4 $processor 29
+    ./manage.* eval_loc_4 $processor 37
+    ./manage.* eval_loc_4 $processor 36
+    ./manage.* eval_loc_4 $processor 35
+    ./manage.* eval_loc_4 $processor 27
+}
 
-./manage.* eval_loc_9 $processor 10
-./manage.* eval_loc_9 $processor 13
-./manage.* eval_loc_9 $processor 34
-./manage.* eval_loc_9 $processor 37
+prev_count=0
+count=$(./manage.* count_segments $processor)
+
+while [ $prev_count -ne $count ];
+do
+    prev_count=$count
+    
+    scan1
+    
+    count=$(./manage.* count_segments $processor)
+    [ $count -eq $prev_count ] && scan2
+
+    count=$(./manage.* count_segments $processor)
+    [ $count -eq $prev_count ] && scan3
+
+    count=$(./manage.* count_segments $processor)
+    [ $count -eq $prev_count ] && scan4
+
+    count=$(./manage.* count_segments $processor)
+    [ $count -eq $prev_count ] && scan5
+        
+    count=$(./manage.* count_segments $processor)
+done
 
 # end of file
