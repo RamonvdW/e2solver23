59fef9760181ec7b9d6eed0c17e3e49b39b39b28
RamonvdW <nhb.ramonvdw@gmail.com>
2023-12-28 10:27:48 +0100

    Extended search

--- ring1_reduce.sh
+++ ring1_reduce.sh
@@ -27,4 +27,27 @@ processor=$1
 ./manage.* eval_loc_1 $processor 34
 ./manage.* eval_loc_1 $processor 42
 
+
+./manage.* eval_loc_4 $processor 10
+./manage.* eval_loc_1 $processor 11
+./manage.* eval_loc_1 $processor 18
+
+./manage.* eval_loc_4 $processor 14
+./manage.* eval_loc_1 $processor 14
+./manage.* eval_loc_1 $processor 23
+
+./manage.* eval_loc_4 $processor 42
+./manage.* eval_loc_1 $processor 42
+./manage.* eval_loc_1 $processor 51
+
+./manage.* eval_loc_4 $processor 46
+./manage.* eval_loc_1 $processor 47
+./manage.* eval_loc_1 $processor 54
+
+
+./manage.* eval_loc_9 $processor 10
+./manage.* eval_loc_9 $processor 13
+./manage.* eval_loc_9 $processor 34
+./manage.* eval_loc_9 $processor 37
+
 # end of file
