3db9404c308dfbc7f3292ce0b05d67d0681004c9
RamonvdW <nhb.ramonvdw@gmail.com>
2024-01-08 21:45:37 +0100

    Better clean-up

--- Pieces2x2/management/commands/drop_segments.py
+++ Pieces2x2/management/commands/drop_segments.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2023 Ramon van der Winkel.
+#  Copyright (c) 2023-2024 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
 from django.core.management.base import BaseCommand
 from Pieces2x2.models import TwoSideOptions
+from WorkQueue.models import Work
 
 
 class Command(BaseCommand):
@@ -27,5 +28,9 @@ class Command(BaseCommand):
         else:
             self.stdout.write('[INFO] Deleting %s records' % count)
             qset.delete()
+
+            count = Work.objects.filter(processor=processor).count()
+            Work.objects.filter(processor=processor).delete()
+            self.stdout.write('[INFO] Deleted %s jobs for processor %s from work queue' % (count, processor))
 
 # end of file
