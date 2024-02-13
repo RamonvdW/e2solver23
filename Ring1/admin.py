52bbfb5664a9d25c2beb69fa05d09a9844a8199e
RamonvdW <nhb.ramonvdw@gmail.com>
2024-02-13 21:18:40 +0100

    Filters

--- Ring1/admin.py
+++ Ring1/admin.py
@@ -8,12 +8,18 @@ from django.contrib import admin
 from Ring1.models import Ring1, Corner1, Corner2, Corner3, Corner4, Corner12, Corner34
 
 
-admin.site.register(Ring1)
-admin.site.register(Corner1)
-admin.site.register(Corner2)
-admin.site.register(Corner3)
-admin.site.register(Corner4)
-admin.site.register(Corner12)
-admin.site.register(Corner34)
+class CornerAdmin(admin.ModelAdmin):
+
+    list_filter = ('seed',)
+
+
+admin.site.register(Ring1, CornerAdmin)
+admin.site.register(Corner1, CornerAdmin)
+admin.site.register(Corner2, CornerAdmin)
+admin.site.register(Corner3, CornerAdmin)
+admin.site.register(Corner4, CornerAdmin)
+admin.site.register(Corner12, CornerAdmin)
+admin.site.register(Corner34, CornerAdmin)
+
 
 # end of file
