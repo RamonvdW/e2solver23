60511494c658af84fe9fc3f7c00f7f6949a73619
RamonvdW <nhb.ramonvdw@gmail.com>
2024-03-14 08:55:31 +0100

    Performance optimizations

--- Ring1/management/commands/calc_bbcbb.py
+++ Ring1/management/commands/calc_bbcbb.py
@@ -55,113 +55,112 @@ class Command(BaseCommand):
             self.twoside2reverse[nr] = rev_nr
         # for
 
-        self.count = 0
         self.bbcbb = list()
-        self._print = 0
+        self._print = -1
 
     def add_arguments(self, parser):
         parser.add_argument('corner', type=int, help='Corner to work on')
 
     def _add_bbcbb(self, bbcbb):
-        self.count += 1
-        if self.count > self._print:
-            print(self.count)
-            self._print += 100
         if bbcbb not in self.bbcbb:
             self.bbcbb.append(bbcbb)
 
+            count = len(self.bbcbb)
+            if count > self._print:
+                print(count)
+                self._print += 10
+
     def _needs_c1(self):
         # solve order: 1, 2, 9, 10
+
+        p10_s1_set = Piece2x2.objects.filter(nr1=208).values_list('side1', flat=True)
+        p10_s4_set = Piece2x2.objects.filter(nr1=208).values_list('side4', flat=True)
+        p2_exp_s3_set = [self.twoside2reverse[side] for side in p10_s1_set]
+        p9_exp_s2_set = [self.twoside2reverse[side] for side in p10_s4_set]
+
         for p1 in Piece2x2.objects.filter(nr1__in=(1, 2, 3, 4)):
             p2_exp_s4 = self.twoside2reverse[p1.side2]
             p9_exp_s1 = self.twoside2reverse[p1.side3]
             used1 = [p1.nr1, p1.nr2, p1.nr3, p1.nr4]
 
-            for p2 in Piece2x2.objects.filter(side4=p2_exp_s4).exclude(nr1__in=used1, nr2__in=used1, nr3__in=used1, nr4__in=used1):
-                p10_exp_s1 = self.twoside2reverse[p2.side3]
+            for p2 in Piece2x2.objects.filter(side4=p2_exp_s4, side3__in=p2_exp_s3_set).exclude(nr1__in=used1, nr2__in=used1, nr3__in=used1, nr4__in=used1):
                 used2 = used1[:]
                 used2.extend([p2.nr1, p2.nr2, p2.nr3, p2.nr4])
 
-                for p9 in Piece2x2.objects.filter(side1=p9_exp_s1).exclude(nr1__in=used2, nr2__in=used2, nr3__in=used2, nr4__in=used2):
-                    p10_exp_s4 = self.twoside2reverse[p9.side2]
-                    used3 = used2[:]
-                    used3.extend([p9.nr1, p9.nr2, p9.nr3, p9.nr4])
-
-                    for p10 in Piece2x2.objects.filter(nr1=208, side1=p10_exp_s1, side4=p10_exp_s4).exclude(nr2__in=used3, nr3__in=used3, nr4__in=used3):
-                        bbcbb = (p9.nr1, p1.nr3, p1.nr2, p1.nr3, p2.nr1)
-                        self._add_bbcbb(bbcbb)
+                for p9 in Piece2x2.objects.filter(side1=p9_exp_s1, side2__in=p9_exp_s2_set).exclude(nr1__in=used2, nr2__in=used2, nr3__in=used2, nr4__in=used2):
+                    bbcbb = (p9.nr1, p1.nr3, p1.nr1, p1.nr2, p2.nr1)
+                    self._add_bbcbb(bbcbb)
                 # for
             # for
         # for
 
     def _needs_c2(self):
         # solve order: 8, 7, 16, 15
+
+        p15_s1_set = Piece2x2.objects.filter(nr2=255).values_list('side1', flat=True)
+        p15_s2_set = Piece2x2.objects.filter(nr2=255).values_list('side2', flat=True)
+        p7_exp_s3_set = [self.twoside2reverse[side] for side in p15_s1_set]
+        p16_exp_s4_set = [self.twoside2reverse[side] for side in p15_s2_set]
+
         for p8 in Piece2x2.objects.filter(nr2__in=(1, 2, 3, 4)):
             p7_exp_s2 = self.twoside2reverse[p8.side4]
             p16_exp_s1 = self.twoside2reverse[p8.side3]
             used1 = [p8.nr1, p8.nr2, p8.nr3, p8.nr4]
 
-            for p7 in Piece2x2.objects.filter(side2=p7_exp_s2).exclude(nr1__in=used1, nr2__in=used1, nr3__in=used1, nr4__in=used1):
-                p15_exp_s1 = self.twoside2reverse[p7.side3]
+            for p7 in Piece2x2.objects.filter(side2=p7_exp_s2, side3__in=p7_exp_s3_set).exclude(nr1__in=used1, nr2__in=used1, nr3__in=used1, nr4__in=used1):
                 used2 = used1[:]
                 used2.extend([p7.nr1, p7.nr2, p7.nr3, p7.nr4])
 
-                for p16 in Piece2x2.objects.filter(side1=p16_exp_s1).exclude(nr1__in=used2, nr2__in=used2, nr3__in=used2, nr4__in=used2):
-                    p15_exp_s2 = self.twoside2reverse[p16.side4]
-                    used3 = used2[:]
-                    used3.extend([p16.nr1, p16.nr2, p16.nr3, p16.nr4])
-
-                    for p15 in Piece2x2.objects.filter(nr2=255, side1=p15_exp_s1, side2=p15_exp_s2).exclude(nr1__in=used3, nr3__in=used3, nr4__in=used3):
-                        bbcbb = (p7.nr2, p8.nr1, p8.nr2, p8.nr4, p16.nr2)
-                        self._add_bbcbb(bbcbb)
+                for p16 in Piece2x2.objects.filter(side1=p16_exp_s1, side4__in=p16_exp_s4_set).exclude(nr1__in=used2, nr2__in=used2, nr3__in=used2, nr4__in=used2):
+                    bbcbb = (p7.nr2, p8.nr1, p8.nr2, p8.nr4, p16.nr2)
+                    self._add_bbcbb(bbcbb)
                 # for
             # for
         # for
 
     def _needs_c3(self):
         # solve order: 64, 63, 56, 55
+
+        p55_s2_set = Piece2x2.objects.filter(nr4=249).values_list('side2', flat=True)
+        p55_s3_set = Piece2x2.objects.filter(nr4=249).values_list('side3', flat=True)
+        p56_exp_s4_set = [self.twoside2reverse[side] for side in p55_s2_set]
+        p63_exp_s1_set = [self.twoside2reverse[side] for side in p55_s3_set]
+
         for p64 in Piece2x2.objects.filter(nr4__in=(1, 2, 3, 4)):
             p63_exp_s2 = self.twoside2reverse[p64.side4]
             p56_exp_s3 = self.twoside2reverse[p64.side1]
             used1 = [p64.nr1, p64.nr2, p64.nr3, p64.nr4]
 
-            for p63 in Piece2x2.objects.filter(side2=p63_exp_s2).exclude(nr1__in=used1, nr2__in=used1, nr3__in=used1, nr4__in=used1):
-                p55_exp_s3 = self.twoside2reverse[p63.side1]
+            for p63 in Piece2x2.objects.filter(side2=p63_exp_s2, side1__in=p63_exp_s1_set).exclude(nr1__in=used1, nr2__in=used1, nr3__in=used1, nr4__in=used1):
                 used2 = used1[:]
                 used2.extend([p63.nr1, p63.nr2, p63.nr3, p63.nr4])
 
-                for p56 in Piece2x2.objects.filter(side3=p56_exp_s3).exclude(nr1__in=used2, nr2__in=used2, nr3__in=used2, nr4__in=used2):
-                    p55_exp_s2 = self.twoside2reverse[p56.side4]
-                    used3 = used2[:]
-                    used3.extend([p56.nr1, p56.nr2, p56.nr3, p56.nr4])
-
-                    for p55 in Piece2x2.objects.filter(nr4=249, side2=p55_exp_s2, side3=p55_exp_s3).exclude(nr1__in=used3, nr2__in=used3, nr3__in=used3):
-                        bbcbb = (p56.nr4, p64.nr2, p64.nr4, p64.nr3, p63.nr4)
-                        self._add_bbcbb(bbcbb)
+                for p56 in Piece2x2.objects.filter(side3=p56_exp_s3, side4__in=p56_exp_s4_set).exclude(nr1__in=used2, nr2__in=used2, nr3__in=used2, nr4__in=used2):
+                    bbcbb = (p56.nr4, p64.nr2, p64.nr4, p64.nr3, p63.nr4)
+                    self._add_bbcbb(bbcbb)
                 # for
             # for
         # for
 
     def _needs_c4(self):
-        # solve order: 57, 58, 50, 49
+        # solve order: 57, 58, 49, 50
+        p50_s3_set = Piece2x2.objects.filter(nr3=181).values_list('side3', flat=True)
+        p50_s4_set = Piece2x2.objects.filter(nr3=181).values_list('side4', flat=True)
+        p58_exp_s1_set = [self.twoside2reverse[side] for side in p50_s3_set]
+        p49_exp_s2_set = [self.twoside2reverse[side] for side in p50_s4_set]
+
         for p57 in Piece2x2.objects.filter(nr3__in=(1, 2, 3, 4)):
             p58_exp_s4 = self.twoside2reverse[p57.side2]
             p49_exp_s3 = self.twoside2reverse[p57.side1]
             used1 = [p57.nr1, p57.nr2, p57.nr3, p57.nr4]
 
-            for p58 in Piece2x2.objects.filter(side4=p58_exp_s4).exclude(nr1__in=used1, nr2__in=used1, nr3__in=used1, nr4__in=used1):
-                p50_exp_s3 = self.twoside2reverse[p58.side1]
+            for p58 in Piece2x2.objects.filter(side4=p58_exp_s4, side1__in=p58_exp_s1_set).exclude(nr1__in=used1, nr2__in=used1, nr3__in=used1, nr4__in=used1):
                 used2 = used1[:]
                 used2.extend([p58.nr1, p58.nr2, p58.nr3, p58.nr4])
 
-                for p50 in Piece2x2.objects.filter(nr3=181, side3=p50_exp_s3).exclude(nr1__in=used2, nr2__in=used2, nr4__in=used2):
-                    p49_exp_s2 = self.twoside2reverse[p50.side4]
-                    used3 = used2[:]
-                    used3.extend([p50.nr1, p50.nr2, p50.nr3, p50.nr4])
-
-                    for p49 in Piece2x2.objects.filter(side2=p49_exp_s2, side3=p49_exp_s3).exclude(nr1__in=used3, nr2__in=used3, nr3__in=used3, nr4__in=used3):
-                        bbcbb = (p58.nr3, p57.nr4, p57.nr3, p57.nr1, p49.nr3)
-                        self._add_bbcbb(bbcbb)
+                for p49 in Piece2x2.objects.filter(side3=p49_exp_s3, side2__in=p49_exp_s2_set).exclude(nr1__in=used2, nr2__in=used2, nr3__in=used2, nr4__in=used2):
+                    bbcbb = (p58.nr3, p57.nr4, p57.nr3, p57.nr1, p49.nr3)
+                    self._add_bbcbb(bbcbb)
                 # for
             # for
         # for
@@ -178,7 +177,6 @@ class Command(BaseCommand):
         elif corner == 4:
             self._needs_c4()
 
-        print('[DEBUG] count = %s' % self.count)
         print('[DEBUG] %s bbcbb:' % len(self.bbcbb))
         for bbcbb in self.bbcbb:
             print(bbcbb)
