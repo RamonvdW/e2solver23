92790ee310bdf1916bcd205aa4495fc91f0dcfb1
RamonvdW <nhb.ramonvdw@gmail.com>
2024-06-14 14:29:38 +0200

    Added eval_claims

--- dup_fix_series.sh
+++ dup_fix_series.sh
@@ -8,18 +8,21 @@ fi
 
 first_board=$1
 last_board=$2
+MANAGE="./manage.py"
 
 echo "[INFO] Running dup_fix_lowest on all boards in the range $first_board to $last_board"
 
 x=$first_board
 while [ $x -le $last_board ]
 do
-    new_boards=$(./manage.py dup_fix_lowest $x)
+    $MANAGE eval_claims $x
+    
+    new_boards=$($MANAGE dup_fix_lowest $x)
     echo "[INFO] Adding scans for boards $new_boards"
     for board in $new_boards
     do
-        ./manage.py add_work $board scan1 2 2
-        ./manage.py add_work $board scan9 9 9
+        $MANAGE add_work $board scan1 2 2
+        $MANAGE add_work $board scan9 9 9
     done
 
     x=$((x+1))
